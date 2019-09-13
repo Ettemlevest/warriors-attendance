cd8c3364431264af50c4e4b49bfde988d2ed252e
Imre Nagy <ettemlevest@outlook.com>
2019-09-13 13:50:15 +0200

    dashboard ui fixes

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -12,24 +12,24 @@
         </h2>
         <div class="text-grey-darker text-sm italic mb-6">{{ training.diff }}</div>
         <div class="sm:block md:block lg:flex xl:flex lg:items-center">
-          <div class="mb-2 flex lg:flex-1 xl:flex-1">
+          <div class="mb-4 flex lg:flex-1 xl:flex-1">
             <icon name="location" class="flex w-5 h-5 fill-grey mr-2" />
             <div class="text-purple text-lg italic">{{ training.place }}</div>
           </div>
-          <div class="text-grey-darker items-center flex lg:flex-1 xl:flex-1">
+          <div class="mb-4 text-grey-darker items-center flex lg:flex-1 xl:flex-1">
             <icon name="dashboard" class="flex w-5 h-5 fill-grey mr-2" />
             <div class="text-lg italic">{{ training.start_at }}</div>
           </div>
-          <div class="text-grey-darker items-center flex lg:flex-1 xl:flex-1">
+          <div class="mb-4 text-grey-darker items-center flex lg:flex-1 xl:flex-1">
             <icon name="users" class="flex w-5 h-5 fill-grey mr-2" />
             <div class="text-lg italic">{{ training.attendees }} / {{ training.max_attendees }}</div>
           </div>
         </div>
-        <button v-if="!training.registered && training.attendees < training.max_attendees" class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 mt-4 rounded">
+        <button v-if="!training.registered && training.attendees < training.max_attendees" class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded">
           Jelentkezem
         </button>
-        <div v-if="!training.registered && training.attendees >= training.max_attendees" class="bg-red text-white font-bold py-2 px-4 mt-4 rounded">Megtelt, már nem lehet jelentkezni!</div>
-        <button v-if="training.registered" class="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 mt-4 rounded">
+        <div v-if="!training.registered && training.attendees >= training.max_attendees" class="bg-red text-white font-bold py-2 px-4 rounded">Megtelt, már nem lehet jelentkezni!</div>
+        <button v-if="training.registered" class="bg-red hover:bg-red-dark text-white font-bold py-2 px-4 rounded">
           Lemondás
         </button>
       </div>
