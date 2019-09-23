0d0ace6e662ab9e245b1224dca13bfbc850c600d
Imre Nagy <ettemlevest@outlook.com>
2019-09-23 13:24:23 +0200

    basic checkbox component

---
+++ resources/js/Shared/CheckboxInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox" :id="id" v-bind="$attrs" :class="{ error: errors.length }" :value="value" @input="$emit('input', $event.target.value)" />
+      {{ label }}
+    </label>
+    <div v-if="errors.length" class="form-error">{{ errors[0] }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    id: {
+      type: String,
+      default() {
+        return `checkbox-input-${this._uid}`
+      },
+    },
+    type: {
+      type: Boolean,
+      default: false,
+    },
+    value: Boolean,
+    label: String,
+    errors: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    focus() {
+      this.$refs.input.focus()
+    },
+    select() {
+      this.$refs.input.select()
+    },
+    setSelectionRange(start, end) {
+      this.$refs.input.setSelectionRange(start, end)
+    },
+  },
+}
+</script>
