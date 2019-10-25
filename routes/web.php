8ff15b0ec7b7e086a0b6c92d09b5c56fb9474ef3
Imre Nagy <ettemlevest@outlook.com>
2019-10-25 08:44:09 +0200

    better route definitions

--- routes/web.php
+++ routes/web.php
@@ -11,35 +11,42 @@
 |
 */
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ImagesController;
+use App\Http\Controllers\TrainingController;
+use App\Http\Controllers\UsersController;
+
 // Auth
-Route::get('login')->name('login')->uses('Auth\LoginController@showLoginForm')->middleware('guest');
-Route::post('login')->name('login.attempt')->uses('Auth\LoginController@login')->middleware('guest');
-Route::get('register')->name('register')->uses('Auth\RegisterController@showRegistrationForm')->middleware('guest');
-Route::post('register')->name('register')->uses('Auth\RegisterController@register')->middleware('guest');
-Route::post('logout')->name('logout')->uses('Auth\LoginController@logout');
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
+Route::post('login', [LoginController::class, 'login'])->name('login.attempt')->middleware('guest');
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register')->middleware('guest');
+Route::post('register', [RegisterController::class, 'register'])->name('register')->middleware('guest');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 // Dashboard
-Route::get('/')->name('dashboard')->uses('DashboardController')->middleware('auth');
+Route::get('/', [DashboardController::class, '__invoke'])->name('dashboard')->middleware('auth');
 
 // Users
-Route::get('users')->name('users')->uses('UsersController@index')->middleware('remember', 'auth');
-Route::get('users/create')->name('users.create')->uses('UsersController@create')->middleware('auth');
-Route::post('users')->name('users.store')->uses('UsersController@store')->middleware('auth');
-Route::get('users/{user}/edit')->name('users.edit')->uses('UsersController@edit')->middleware('auth');
-Route::put('users/{user}')->name('users.update')->uses('UsersController@update')->middleware('auth');
-Route::delete('users/{user}')->name('users.destroy')->uses('UsersController@destroy')->middleware('auth');
-Route::put('users/{user}/restore')->name('users.restore')->uses('UsersController@restore')->middleware('auth');
+Route::get('users', [UsersController::class, 'index'])->name('users')->middleware('remember', 'auth');
+Route::get('users/create', [UsersController::class, 'create'])->name('users.create')->middleware('auth');
+Route::post('users', [UsersController::class, 'store'])->name('users.store')->middleware('auth');
+Route::get('users/{user}/edit', [UsersController::class, 'edit'])->name('users.edit')->middleware('auth');
+Route::put('users/{user}', [UsersController::class, 'update'])->name('users.update')->middleware('auth');
+Route::delete('users/{user}', [UsersController::class, 'destroy'])->name('users.destroy')->middleware('auth');
+Route::put('users/{user}/restore', [UsersController::class, 'restore'])->name('users.restore')->middleware('auth');
 
 // Images
-Route::get('/img/{path}', 'ImagesController@show')->where('path', '.*');
+Route::get('/img/{path}', [ImagesController::class, 'show'])->where('path', '.*');
 
 // Trainings
-Route::get('trainings')->name('trainings')->uses('TrainingController@index')->middleware('remember', 'auth');
-Route::get('trainings/create')->name('trainings.create')->uses('TrainingController@create')->middleware('auth');
-Route::get('trainings/{training}/view')->name('trainings.view')->uses('TrainingController@view')->middleware('auth');
-Route::post('trainings')->name('trainings.store')->uses('TrainingController@store')->middleware('auth');
-Route::get('trainings/{training}/edit')->name('trainings.edit')->uses('TrainingController@edit')->middleware('auth');
-Route::put('trainings/{training}')->name('trainings.update')->uses('TrainingController@update')->middleware('auth');
-Route::delete('trainings/{training}')->name('trainings.destroy')->uses('TrainingController@destroy')->middleware('auth');
-Route::post('trainings/{training}/attend')->name('trainings.attend')->uses('TrainingController@attend')->middleware('auth');
-Route::delete('trainings/{training}/withdraw')->name('trainings.withdraw')->uses('TrainingController@withdraw')->middleware('auth');
+Route::get('trainings', [TrainingController::class, 'index'])->name('trainings')->middleware('remember', 'auth');
+Route::get('trainings/create', [TrainingController::class, 'create'])->name('trainings.create')->middleware('auth');
+Route::get('trainings/{training}/view', [TrainingController::class, 'view'])->name('trainings.view')->middleware('auth');
+Route::post('trainings', [TrainingController::class, 'store'])->name('trainings.store')->middleware('auth');
+Route::get('trainings/{training}/edit', [TrainingController::class, 'edit'])->name('trainings.edit')->middleware('auth');
+Route::put('trainings/{training}', [TrainingController::class, 'update'])->name('trainings.update')->middleware('auth');
+Route::delete('trainings/{training}', [TrainingController::class, 'destroy'])->name('trainings.destroy')->middleware('auth');
+Route::post('trainings/{training}/attend', [TrainingController::class, 'attend'])->name('trainings.attend')->middleware('auth');
+Route::delete('trainings/{training}/withdraw', [TrainingController::class, 'withdraw'])->name('trainings.withdraw')->middleware('auth');
