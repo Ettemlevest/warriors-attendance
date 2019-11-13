13c1d73ec4db42949d3230c4eadf55fb598ce683
Imre Nagy <ettemlevest@outlook.com>
2019-11-13 16:52:19 +0100

    bugfix for small screens

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -12,8 +12,6 @@
               <svg class="fill-white w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
               <div class="flex flex-grow overflow-hidden">
                 <main-menu class="bg-indigo-darker flex-no-shrink w-56 p-12 hidden md:block overflow-y-auto" />
-                <flash-messages />
-                <slot />
               </div>
             </dropdown>
           </div>
