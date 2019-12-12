5779513f548ea098e952612355606455588e21fb
Imre Nagy <ettemlevest@outlook.com>
2019-12-12 15:06:32 +0100

    fixed dashboard ui problems

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -3,9 +3,9 @@
     <h1 class="mb-8 font-bold text-3xl">Üdvözlünk!</h1>
     <p class="mb-12 leading-normal">Szia Warrior! Itt jelentkezhetsz a következő edzésekre.</p>
 
-    <div v-if="this.message" class="mb-8 flex items-center justify-between bg-blue rounded max-w">
+    <div v-if="this.message" class="mb-8 flex items-center justify-between bg-blue-500 rounded max-w">
       <div class="flex items-center">
-        <icon name="notifications" class="ml-8 mr-4 flex-shrink-0 w-8 h-8 fill-white" />
+        <icon name="notifications" class="ml-8 mr-4 flex-shrink-0 w-8 h-8 fill-current text-white" />
         <div class="py-4 text-white text-sm font-medium">
           <p class="font-bold text-xl mb-2">{{ this.message.title }}</p>
           {{ this.message.body }}
@@ -17,33 +17,32 @@
     <p v-if="trainings.length === 0" class="mb-12 leading-normal">Egyelőre nincs felvéve új edzés. Nézz vissza később 😊</p>
     <div v-for="training in trainings" :key="training.id" class="overflow-x-auto block lg:flex-wrap xl:flex-wrap">
       <div class="text-center sm:w-auto md:w-auto lg:flex-1 xl:flex-1 m-4 p-4 bg-white rounded-lg shadow-md">
-        <h2 class="text-xl mb-2 ml-8 mr-8">
-          <inertia-link class="text-indigo-600" :href="route($page.auth.user.owner ? 'trainings.edit' : 'trainings.view', training.id)">{{ training.name }}</inertia-link>
+        <h2 class="text-xl mb-2 ml-8 mr-8 font-bold">
+          <inertia-link class="text-indigo-700" :href="route($page.auth.user.owner ? 'trainings.edit' : 'trainings.view', training.id)">{{ training.name }}</inertia-link>
         </h2>
         <div class="text-gray-600 text-sm italic mb-6">{{ training.diff }}</div>
         <div class="sm:block md:block lg:flex xl:flex lg:items-center">
           <div class="mb-4 flex lg:flex-1 xl:flex-1">
-            <icon name="location" class="flex w-5 h-5 fill-gray mr-2" />
-            <div class="text-purple text-lg italic">{{ training.place }}</div>
+            <icon name="location" class="flex w-5 h-5 fill-current text-gray-500 mr-2" />
+            <div class="text-purple-600 text-lg italic">{{ training.place }}</div>
           </div>
           <div class="mb-4 text-gray-600 items-center flex lg:flex-1 xl:flex-1">
-            <icon name="dashboard" class="flex w-5 h-5 fill-gray mr-2" />
+            <icon name="dashboard" class="flex w-5 h-5 fill-current text-gray-500 mr-2" />
             <div class="text-lg italic">{{ training.start_at }}</div>
           </div>
           <div class="mb-4 text-gray-600 items-center flex lg:flex-1 xl:flex-1">
-            <icon name="users" class="flex w-5 h-5 fill-gray mr-2" />
+            <icon name="users" class="flex w-5 h-5 fill-current text-gray-500 mr-2" />
             <div class="text-lg italic">{{ training.attendees.length }} / {{ training.max_attendees || '&infin;' }}</div>
           </div>
         </div>
-        <div></div>
-        <button v-if="!training.registered && (training.max_attendees === 0 || training.attendees.length < training.max_attendees)" class="bg-blue hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" @click="attend(training.id)">
+        <button v-if="!training.registered && (training.max_attendees === 0 || training.attendees.length < training.max_attendees)" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="attend(training.id)">
           Jelentkezem
         </button>
-        <div v-if="!training.registered && training.attendees.length >= training.max_attendees && !training.can_attend_more" class="bg-red text-white font-bold py-2 px-4 rounded">Megtelt, már nem lehet jelentkezni!</div>
-        <button v-if="training.registered" class="bg-red hover:bg-red-600 text-white font-bold py-2 px-4 rounded" @click="withdraw(training.id)">
+        <div v-if="!training.registered && training.attendees.length >= training.max_attendees && !training.can_attend_more" class="bg-red-600 text-white font-bold py-2 px-4 rounded">Megtelt, már nem lehet jelentkezni!</div>
+        <button v-if="training.registered" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" @click="withdraw(training.id)">
           Lemondás
         </button>
-        <button v-if="!training.registered && training.max_attendees && training.attendees.length >= training.max_attendees && training.can_attend_more" class="bg-blue hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" @click="attend(training.id)">
+        <button v-if="!training.registered && training.max_attendees && training.attendees.length >= training.max_attendees && training.can_attend_more" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="attend(training.id)">
           Megtelt, mégis jelentkezem. Vállalom a 10 burpeet beugrásnak!
         </button>
       </div>
