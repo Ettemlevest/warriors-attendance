604184b9910f6ca650c1393902e42fe2a1f2efc6
Imre Nagy <ettemlevest@outlook.com>
2019-12-12 09:31:12 +0100

    color fixes

--- resources/js/Shared/TextInput.vue
+++ resources/js/Shared/TextInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label v-if="label" class="form-label" :for="id">{{ label }}:</label>
+    <label v-if="label" class="form-label text-gray-800" :for="id">{{ label }}:</label>
     <input :id="id" ref="input" v-bind="$attrs" class="form-input" :class="{ error: errors.length }" :type="type" :value="value" @input="$emit('input', $event.target.value)">
     <div v-if="errors.length" class="form-error">{{ errors[0] }}</div>
   </div>
