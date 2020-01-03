4f8629843eb21eed3bcad3ffdc4069adaf273408
Imre Nagy <ettemlevest@outlook.com>
2020-01-03 10:52:03 +0100

    added often used templates to training creation

--- resources/js/Pages/Trainings/Create.vue
+++ resources/js/Pages/Trainings/Create.vue
@@ -5,9 +5,20 @@
       <span class="text-indigo-500 font-medium">/</span> Létrehozás
     </h1>
     <div v-if="$page.auth.user.owner" class="bg-white rounded shadow overflow-hidden max-w">
+      <div class="px-8 py-4">
+        <button
+          v-for="template in templates"
+          :key="template"
+          class="btn-indigo inline-block rounded-full px-4 py-2 text-sm font-semibold mr-2"
+          @click="setTemplate(template)"
+        >
+          {{ template.template }}
+        </button>
+      </div>
+
       <form @submit.prevent="submit">
-        <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
-        <text-input v-model="form.name" :errors="$page.errors.name" class="pr-6 pb-8 w-full lg:w-1/2" label="Edzés neve" autofocus />
+        <div class="px-8 py-4 -mr-6 -mb-8 flex flex-wrap">
+          <text-input v-model="form.name" :errors="$page.errors.name" class="pr-6 pb-8 w-full lg:w-1/2" label="Edzés neve" autofocus />
           <text-input v-model="form.place" :errors="$page.errors.place" class="pr-6 pb-8 w-full lg:w-1/2" label="Helyszín" />
           <text-input v-model="form.start_at_day" :errors="$page.errors.start_at_day" class="pr-6 pb-8 w-full lg:w-1/2" label="Kezdés (dátum)" type="date" timezone="Europe/Budapest" />
           <text-input v-model="form.start_at_time" :errors="$page.errors.start_at_time" class="pr-6 pb-8 w-full lg:w-1/2" label="Kezdés (időpont)" type="time" timezone="Europe/Budapest" />
@@ -43,6 +54,35 @@ export default {
   data() {
     return {
       sending: false,
+      templates: [
+        {
+          template: 'Köredzés 18:45',
+          name: 'Köredzés',
+          place: 'Sportcsarnok',
+          start_at_time: '18:45',
+          length: '60',
+          max_attendees: '36',
+          can_attend_more: true,
+        },
+        {
+          template: 'Köredzés 20:00',
+          name: 'Köredzés',
+          place: 'Sportcsarnok',
+          start_at_time: '20:00',
+          length: '60',
+          max_attendees: '36',
+          can_attend_more: true,
+        },
+        {
+          template: 'Köredzés 19:45',
+          name: 'Köredzés',
+          place: 'Sportcsarnok',
+          start_at_time: '19:45',
+          length: '60',
+          max_attendees: '36',
+          can_attend_more: true,
+        },
+      ],
       form: {
         name: null,
         place: null,
@@ -70,6 +110,14 @@ export default {
 
       this.$inertia.post(this.route('trainings.store'), data)
         .then(() => this.sending = false)
+    },
+    setTemplate(template) {
+      this.form.name = template.name
+      this.form.place = template.place
+      this.form.start_at_time = template.start_at_time
+      this.form.length = template.length
+      this.form.max_attendees = template.max_attendees
+      this.form.can_attend_more = template.can_attend_more
     }
   },
 }
