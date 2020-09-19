b1654031a3440664fe9517ac415fbd9f6bab0888
Imre Nagy <i.nagy23@outlook.com>
2020-09-19 16:33:55 +0200

    added analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,17 @@
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178463144-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-178463144-1');
+    </script>
+
 </head>
 <body class="font-sans leading-none text-gray-800 antialiased">
 
