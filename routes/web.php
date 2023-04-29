35b80562c7a82e93284be45e8b370a5b6aa05981
Imre Nagy <i.nagy23@outlook.com>
2023-04-29 10:42:23 +0200

    add lowercase middleware to user action endpoints

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,10 @@ Route::get('/', [DashboardController::class, '__invoke'])->name('dashboard')->mi
 
 // Users
 Route::get('users', [UsersController::class, 'index'])->name('users')->middleware('remember', 'auth');
-Route::get('users/create', [UsersController::class, 'create'])->name('users.create')->middleware('auth');
+Route::get('users/create', [UsersController::class, 'create'])->name('users.create')->middleware('auth', 'lowercase');
 Route::post('users', [UsersController::class, 'store'])->name('users.store')->middleware('auth');
 Route::get('users/{user}/edit', [UsersController::class, 'edit'])->name('users.edit')->middleware('auth');
-Route::put('users/{user}', [UsersController::class, 'update'])->name('users.update')->middleware('auth');
+Route::put('users/{user}', [UsersController::class, 'update'])->name('users.update')->middleware('auth', 'lowercase');
 Route::delete('users/{user}', [UsersController::class, 'destroy'])->name('users.destroy')->middleware('auth');
 Route::put('users/{user}/restore', [UsersController::class, 'restore'])->name('users.restore')->middleware('auth');
 
