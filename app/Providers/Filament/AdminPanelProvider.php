75a22ec4e416b39f74d293fed5fce00531098c99
Imre Nagy <i.nagy23@outlook.com>
2023-09-16 10:44:05 +0200

    customise filament panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,8 +27,13 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
 
             ->login()
+            ->registration()
+            ->passwordReset()
+            ->emailVerification()
+            ->profile()
+
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Green,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
