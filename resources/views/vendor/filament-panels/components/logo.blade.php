19dc5c01b6beb5e221fd00361f80f934fc4f6404
Imre Nagy <i.nagy23@outlook.com>
2023-09-18 19:51:04 +0200

    fix

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center">
-    <img src="{{ asset('/logo.png') }}" alt="Logo" class="h-10 mr-2">
+    <img src="{{ asset('/logo.png') }}" alt="Logo" class="h-10">
 
-    <span class="ml-2">KKL Warriors</span>
+    <span class="px-4 font-bold">KKL Warriors</span>
 </div>
 
