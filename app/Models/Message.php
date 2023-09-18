f2d5a2a25f11b9e846241494b43ba724adbc9eda
Imre Nagy <i.nagy23@outlook.com>
2023-09-18 20:00:48 +0200

    generate model docblock

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Message
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $body
+ * @property \Illuminate\Support\Carbon|null $showed_from
+ * @property \Illuminate\Support\Carbon|null $showed_to
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Message newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Message newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Message query()
+ *
+ * @mixin Model
+ */
 class Message extends Model
 {
     protected $casts = [
--- app/Models/Training.php
+++ app/Models/Training.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * App\Models\Training
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $place
+ * @property \Illuminate\Support\Carbon $start_at
+ * @property int $length
+ * @property int $max_attendees
+ * @property bool $can_attend_more
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $type
+ * @property string|null $description
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, User> $attendees
+ * @property-read int|null $attendees_count
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Training newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Training newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Training query()
+ *
+ * @mixin Model
+ */
 final class Training extends Model
 {
     protected $casts = [
