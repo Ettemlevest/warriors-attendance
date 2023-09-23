84d7db2e50030612f366e2b0d71b2c0e0f7d63a7
Imre Nagy <i.nagy23@outlook.com>
2023-09-23 20:34:36 +0200

    additional actions for training resource

--- app/Filament/Resources/TrainingResource.php
+++ app/Filament/Resources/TrainingResource.php
@@ -198,6 +198,7 @@ class TrainingResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
